27ba8a80e2abbd7df490cfa6a3e6d10fdb4a4a74
michaelseemichaeldo <michael.candreia@gmail.com>
2020-01-10 00:32:23 +0100

    wrote code

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -11,6 +11,7 @@ contract SimpleBank {
     //
     // State variables
     //
+    address payable customer;
     
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
     mapping (address => uint) balances;
@@ -26,14 +27,14 @@ contract SimpleBank {
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     //
     // Functions
@@ -42,6 +43,7 @@ contract SimpleBank {
     /* Use the appropriate global variable to get the sender of the transaction */
     constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -57,14 +59,18 @@ contract SimpleBank {
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
+    function enroll() public returns (bool) {
+        enrolled[msg.sender]==true;
+        emit LogEnrolled(msg.sender);
+        return  enrolled[msg.sender];
     }
 
     /// @notice Deposit ether into bank
@@ -73,9 +79,12 @@ contract SimpleBank {
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
     // Users should be enrolled before they can make deposits
-    function deposit() public returns (uint) {
+    function deposit(uint amount) public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        balances[msg.sender]+=amount;
+        emit LogDepositMade(msg.sender, amount);
+        return  balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -84,6 +93,11 @@ contract SimpleBank {
     /// @return The balance remaining for the user
     // Emit the appropriate event    
     function withdraw(uint withdrawAmount) public returns (uint) {
+        require (balances[msg.sender] >= withdrawAmount,"Insufficient Balance");
+        balances[msg.sender]-=withdrawAmount;
+        msg.sender.transfer(withdrawAmount);    
+        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+        return balances[msg.sender];
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
